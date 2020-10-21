91c5aa38d30e447578a95ff194205325fcd3e0b7
amshel hack3r <amshelhack3r@gmail.com>
2020-10-21 21:52:55 +0300

    wrap app in a provider

--- client/src/index.js
+++ client/src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { ThemeProvider } from './state/ThemeContext';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <ThemeProvider>
     <App />
-  </React.StrictMode>,
+  </ThemeProvider>,
   document.getElementById('root')
 );
