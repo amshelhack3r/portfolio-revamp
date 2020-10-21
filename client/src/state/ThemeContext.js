152aab779c850375101df331c6a83b41edaf7c84
amshel hack3r <amshelhack3r@gmail.com>
2020-10-21 21:55:50 +0300

    Create theme provider context

---
+++ client/src/state/ThemeContext.js
@@ -0,0 +1,44 @@
+import React, { useReducer } from 'react'
+import { darkTheme, lightTheme } from './theme'
+
+
+const initialState = {
+    isDark: true,
+    theme: darkTheme
+};
+
+
+const ThemeContext = React.createContext(initialState)
+
+const reducer = (state, action) => {
+    switch (action.type) {
+        case "CHANGE_THEME":
+            console.log(state);
+            return state.isDark ? {
+                isDark: false,
+                theme: lightTheme
+            } : {
+                    isDark: true,
+                    theme: darkTheme
+                }
+        default:
+            return state;
+    }
+}
+
+export const ThemeProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState)
+
+    const changeTheme = () => dispatch({ type: "CHANGE_THEME" });
+
+    return (
+        <ThemeContext.Provider value={{
+            changeTheme,
+            theme: state.theme
+        }}>
+            {children}
+        </ThemeContext.Provider>
+    )
+}
+
+export default ThemeContext;
