ecb83edaf077da04c75a34f83f0197a88d031405
amshel hack3r <amshelhack3r@gmail.com>
2020-10-22 00:04:39 +0300

    add light and dark themes for site

--- client/src/state/theme.js
+++ client/src/state/theme.js
@@ -2,7 +2,9 @@ const globals = {
     primary: "#081229",
     secondary: "#0F101A",
     outline_red: "#BF1650",
-    whitesmoke: "#f5f5f5"
+    whitesmoke: "#f5f5f5",
+    primary_accent: "#191D3A"
+
 
 }
 //write my dark themes here
@@ -13,11 +15,13 @@ export const darkTheme = {
         fontFamily: `'Cantarell', sans-serif`,
         color: "white",
     },
+    hero: {
+        backgroundColor: globals.primary_accent,
+        boxShadow: `0 2px 8px 2px ${globals.outline_red}`
+    },
     avatar: "images/avatar.jpg",
-    avatarCard: {
-        width: "350",
-        // border: `2px solid ${globals.outline_red}`,
-        boxShadow: `0 19px 38px ${globals.outline_red}, 0 15px 12px ${globals.outline_red}`
+    avatarOutline: {
+        border: `1px solid ${globals.outline_red}`
     },
     about: {
         fontSize: '28px',
@@ -29,7 +33,21 @@ export const darkTheme = {
         borderTop: `medium double ${globals.whitesmoke}`,
         color: globals.whitesmoke
 
+    },
+    heroText: {
+        color: globals.whitesmoke,
+        textAlign: "center",
+        fontWeight: "700"
+    },
+    heroBottom: {
+        backgroundColor: globals.primary
+    },
+    stack_span: {
+        border: `1px solid ${globals.outline_red}`
+
+
     }
+
 
 }
 
@@ -41,21 +59,38 @@ export const lightTheme = {
         color: "#0F101A",
     },
     avatar: "images/avatar_light.jpg",
-    avatarCard: {
-        width: "350",
-        border: `2px solid ${globals.primary}`,
-        boxShadow: `0 19px 38px ${globals.primary}, 0 15px 12px ${globals.primary}`
+    hero: {
+        backgroundColor: "#F5F5F5",
+        boxShadow: `0 2px 8px 2px ${globals.primary}`
+    },
+    avatarOutline: {
+        border: `1px solid ${globals.primary}`
     },
     about: {
         fontSize: '28px',
         lineHeight: '44px',
         textAlign: 'center'
     },
-    smiley: globals.outline_red,
+    smiley: "#1890FF",
     hr: {
         borderTop: `medium double ${globals.outline_red}`,
         color: globals.outline_red
 
+    },
+    heroText: {
+        color: globals.primary,
+        textAlign: "center"
+    },
+    heroBottom: {
+        backgroundColor: "#dbedf3",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "flex-start"
+
+    },
+    stack_span: {
+        border: `1px solid ${globals.primary}`
     }
+
 }
 
