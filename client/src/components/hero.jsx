33514c45ae2f005e9ba197330b272fcd47e3ce50
amshel hack3r <amshelhack3r@gmail.com>
2020-10-22 00:03:46 +0300

    Created hero card, Looks good :) Very Good

--- client/src/components/hero.jsx
+++ client/src/components/hero.jsx
@@ -1,23 +1,44 @@
 import React from 'react'
-import { Row, Col, Image, Card, Typography } from 'antd'
+import { Row, Col, Image, Button } from 'antd'
 import { SmileTwoTone } from '@ant-design/icons';
 import { useContext } from 'react';
 import ThemeContext from '../state/ThemeContext';
 
-const { Title } = Typography
-const { Meta } = Card;
 export default function Hero() {
 
     const { theme } = useContext(ThemeContext)
     return <Row className="hero">
         <Col span="6">
-            <Card
-                hoverable
-                style={theme.avatarCard}
-                cover={<img alt="example" src={theme.avatar} />}
-            >
-                <Meta title="Samuel Kanyi" description="Full-Stack Engineer" />
-            </Card>
+            <div className="hero_card" style={theme.hero}>
+                <div className="hero_card__upper" style={theme.heroUpper}>
+                    <div className="image">
+                        <Image
+                            style={theme.avatarOutline}
+                            className="avatar"
+                            width={150}
+                            src={theme.avatar}
+                            fallback="images/404error.jpg"
+                        />
+                    </div>
+                    <h2 style={theme.heroText}>Samuel Kanyi</h2>
+                    <h3 style={theme.heroText}>Kenya</h3>
+                    <p style={theme.heroText}>Android Developer and Full-Stack Engineer</p>
+                </div>
+                <div className="hero_card__lower" style={theme.heroBottom}>
+                    <h4 style={theme.heroText}>Skills</h4>
+                    <div className="skills_button">
+                        <span style={theme.stack_span}>UI/UX</span>
+                        <span style={theme.stack_span}>Javascript</span>
+                        <span style={theme.stack_span}>React</span>
+                        <span style={theme.stack_span}>Node</span>
+                        <span style={theme.stack_span}>Android</span>
+                        <span style={theme.stack_span}>Kotlin</span>
+                        <span style={theme.stack_span}>Java</span>
+                        <span style={theme.stack_span}>Dart</span>
+                        <span style={theme.stack_span}>Front End Development</span>
+                    </div>
+                </div>
+            </div>
         </Col>
         <Col span="2"></Col>
         <Col span="16">
