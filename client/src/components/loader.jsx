fd027ec49a898e6e6933e8b0bd7f557228bcc0e6
amshel hack3r <amshelhack3r@gmail.com>
2020-10-22 18:53:15 +0300

    display an awesome loader

--- client/src/components/loader.jsx
+++ client/src/components/loader.jsx
@@ -1,16 +1,13 @@
 import React from 'react'
-import '../scss/loader.scss'
+
 export default function Loader() {
-    return <div id="container">
-        <div className="divider" aria-hidden="true"></div>
-        <p className="loading-text" aria-label="Loading">
-            <span className="letter" aria-hidden="true">L</span>
-            <span className="letter" aria-hidden="true">o</span>
-            <span className="letter" aria-hidden="true">a</span>
-            <span className="letter" aria-hidden="true">d</span>
-            <span className="letter" aria-hidden="true">i</span>
-            <span className="letter" aria-hidden="true">n</span>
-            <span className="letter" aria-hidden="true">g</span>
-        </p>
+    return <div className="wrapper">
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="circle"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+        <div className="shadow"></div>
+        <span>Loading</span>
     </div>
 }
